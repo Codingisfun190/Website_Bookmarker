cb96f94b66c80283fede2d9e3637207d9852eaf2
Codingisfun190 <chanyoung720@gmail.com>
2023-01-20 17:14:42 -0500

    put visit and delete buttons, made sure website url is a real url, reset forms after entry

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ function saveBookmark(e) {
   var siteName = document.getElementById('siteName').value
   var siteUrl = document.getElementById('siteUrl').value
 
+  if (!validateForm(siteName, siteUrl)) {
+    return false
+  }
+
   var bookmark = {
     name: siteName,
     url: siteUrl
@@ -37,8 +41,32 @@ function saveBookmark(e) {
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
   }
 
+  // Clear form
+  document.getElementById('myForm').reset()
+
+  // Re-fetch bookmarks
+  fetchBookmarks()
+
   // Prevent form from submitting
   e.preventDefault()
+}
+
+// Delete bookmark
+function deleteBookmark(url) {
+  // Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
+  // Loop through bookmarks
+  for (var i = 0; i < bookmarks.length; i++) {
+    if (bookmarks[i].url === url) {
+      // Remove from array
+      bookmarks.splice(i, 1)
+    }
+  }
+  // Re-set back to localStorage
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+
+  // Re-fetch bookmarks
+  fetchBookmarks()
 }
 
 // Fetch bookmarks
@@ -56,6 +84,36 @@ function fetchBookmarks() {
     var url = bookmarks[i].url
 
     bookmarksResults.innerHTML +=
-      `<div class="well">` + `<h3>` + name + `</h3>` + `</div>`
+      `<div class="well">` +
+      `<h3>` +
+      name +
+      `<a class="btn btn-default" target="_blank" href="` +
+      url +
+      `">Visit</a>` +
+      `<a onclick="deleteBookmark(\`` +
+      url +
+      `\`)"class="btn btn-danger" href="#
+      ">Delete</a>` +
+      `</h3>` +
+      `</div>`
   }
+}
+
+// Validate form
+function validateForm(siteName, siteUrl) {
+  if (!siteName || !siteUrl) {
+    alert('Please fill in the form')
+    return false
+  }
+
+  var expression =
+    /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi
+  var regex = new RegExp(expression)
+
+  if (!siteUrl.match(regex)) {
+    alert('Please use a valid URL')
+    return false
+  }
+
+  return true
 }
