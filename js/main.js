766977923e9a1909daabe3b8e232414c15f58d63
Codingisfun190 <chanyoung720@gmail.com>
2023-01-20 12:36:39 -0500

    worked on some JS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 // Listen for form submit
 document.getElementById('myForm').addEventListener('submit', saveBookmark)
 
+// Save bookmark
 function saveBookmark(e) {
-  console.log('It works')
+  // Get form values
+  var siteName = document.getElementById('siteName').value
+  var siteUrl = document.getElementById('siteUrl').value
 
+  var bookmark = {
+    name: siteName,
+    url: siteUrl
+  }
+
+  /*
+  // Local storage test
+  localStorage.setItem('test', 'Hello World')
+  console.log(localStorage.getItem('test'))
+  localStorage.removeItem('test')
+  console.log(localStorage.getItem('test'))
+  */
+
+  // Prevent form from submitting
   e.preventDefault()
 }
