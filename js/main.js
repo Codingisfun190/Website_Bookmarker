efb49749bff3769e3fc2eda4fe00e515a9ee2ab0
Codingisfun190 <chanyoung720@gmail.com>
2023-01-20 12:57:17 -0500

    worked more on JS

--- js/main.js
+++ js/main.js
@@ -20,6 +20,42 @@ function saveBookmark(e) {
   console.log(localStorage.getItem('test'))
   */
 
+  // Test if bookmarks is null
+  if (localStorage.getItem('bookmarks') === null) {
+    // Initialize array
+    var bookmarks = []
+    // Add to array
+    bookmarks.push(bookmark)
+    // Set to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+  } else {
+    // Get bookmarks from localStorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
+    // Add bookmark to array
+    bookmarks.push(bookmark)
+    // Re-set back to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+  }
+
   // Prevent form from submitting
   e.preventDefault()
+}
+
+// Fetch bookmarks
+function fetchBookmarks() {
+  // Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
+
+  // Get output id
+  var bookmarksResults = document.getElementById('bookmarksResults')
+
+  // Build output
+  bookmarksResults.innerHTML = ''
+  for (var i = 0; i < bookmarks.length; i++) {
+    var name = bookmarks[i].name
+    var url = bookmarks[i].url
+
+    bookmarksResults.innerHTML +=
+      `<div class="well">` + `<h3>` + name + `</h3>` + `</div>`
+  }
 }
